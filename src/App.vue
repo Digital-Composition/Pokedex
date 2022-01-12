950fe774989496d603fc3d0dfc2ef35a46c2f26b
Alex <ahperez@gwggroup.com>
2022-01-11 18:10:46 -0800

    Initial Stage

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="app" class="container">
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: "App",
+		components: {},
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	@import "./css/flex-style.css";
+	@import "./css/font-style.css";
+	@import "./css/size-position-style.css";
+
+	html {
+		background-image: url("~@/assets/app-bg.svg");
+		background-attachment: fixed;
+		background-position: center;
+	}
 </style>
